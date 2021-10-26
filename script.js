dcd81393e4a9bdbc852b6659b52b223497e9809b
carolina ariadne <c.ariadneadv@gmail.com>
2021-10-26 12:00:41 -0300

    requisito-7

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
 
-let selecionandoCoresPaletas = document.getElementsByClassName('color') //selecionando paletas
+let selecionandoCoresPaletas = document.getElementById('color-palette') //selecionando paleta
 
-function adicionandoClickPaleta() { // adicionando evento de click nas divs paleta
+function corSelecionada(event) { //Quando passo o event como parâmetro na função, eu estou passando o evento do clique como parâmetro,  referente ao quadrado clicado.
 
-    for (let index1 = 0; index1 < selecionandoCoresPaletas.length; index1 += 1) { ////cada div da paleta,  em sua posição,recebe o evento de click (ouvidor), após, função que dita o que ocorre pós click (seleção de cor para poder ser usada nos pixels)
+    let guardando = event.target; ////meu elemento div (target) que está recebendo o event click 
 
-        selecionandoCoresPaletas[index1].addEventListener('click', corSelecionada)
+    let selecionandoElementoComSelected = document.querySelector('.selected'); // selecionando elemento que possui a classe
+
+    selecionandoElementoComSelected.classList.remove('selected');
+
+    guardando.classList.add('selected'); //elemento clicado(div), adicionando classe selected nela
+
+}
+
+function adicionandoClickPaleta() { // adicionando evento de click paleta
+
+    for (let index1 = 0; index1 < selecionandoCoresPaletas.children.length; index1 += 1) { //pegando tamanho dos filhos da div color palette
+
+        selecionandoCoresPaletas.children[index1].addEventListener('click', corSelecionada); ////cada div da paleta,  em sua posição,recebe o evento de click (ouvidor)
 
     }
-
 }
-
-
-function corSelecionada(event) { //cliquei no quadrado. Quando passo o event como parâmetro na função, eu estou passando o evento do clique como parâmetro,  referente ao quadrado clicado.
-
-    let guardando = event.target; ////meu elemento div (target) que está recebendo o event click neste momento(ouvindo o click que ocorreu na página), está sendo guardado na variável passada
-
-    selecionandoCoresPaletas.document.getElementsByClassName('selected'); // selecionando elemento que possui a classe
-    selecionandoCoresPaletas.remove('selected'); //removendo a classe do elemento
-
-    guardando.className = 'selected'; //adicionando a classe ao elemento que recebeu o evento de click
-
-
-}
+adicionandoClickPaleta();
 
 
 
+//A classe selected deve ser adicionada à cor selecionada na paleta, ao mesmo tempo em que é removida da cor anteriormente selecionada;
 
+//A classe selected deve ser adicionada à cor selecionada na paleta, ao mesmo tempo em que é removida da cor anteriormente selecionada;
+
+//Somente uma das cores da paleta deve ter a classe selected de cada vez;
+
+//Note que os elementos que deverão receber a classe selected devem ser os mesmos elementos que possuem a classe color, como especificado no requisito 2.
 
 
 
